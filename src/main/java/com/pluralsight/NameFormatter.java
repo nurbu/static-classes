337d029c6ec1acce55b97857d0e9f9b282248587
Nurbu <lamanurbu2@gmail.com>
2026-05-06 13:45:13 -0400

    Added logic for format(fullName)

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -22,7 +22,45 @@ public class NameFormatter {
         }
         return fullName;
     }
-    
+
+    public static String format(String fullName) {
+        String[] split = fullName.split(" ");
+        boolean prefix = false;
+        boolean middle = false;
+        boolean suffix = false;
+        String firstName = "";
+        String lastName = "";
+        String middleName = "";
+
+        if (split.length == 2) {
+            return split[1] + ", " + split[0];
+        }
+
+        if (split[split[0].length() - 1] == ".") {
+            prefix = true;
+            firstName = split[1];
+        }
+        int secondLast = split.length - 2;
+        String value = split[secondLast];
+        if (value.charAt(value.length() - 1) == ',') {
+            suffix = true;
+            lastName = split[secondLast];
+            if (!(firstName.equals(split.length - 3))) {
+                middle = true;
+                middleName = split[secondLast - 1];
+            }
+        } else lastName = split[split.length - 1];
+
+        fullName += lastName + ", ";
+        if (prefix) fullName += split[0];
+        fullName += firstName + " ";
+        if (middle) fullName += middleName;
+        fullName += lastName;
+        if (suffix) fullName += split[secondLast] + 1;
+        return fullName;
+
+    }
+
 
 }
 
