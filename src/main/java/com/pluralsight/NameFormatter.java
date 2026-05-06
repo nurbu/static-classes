b29e6f9030987423f4cf7207f52a52b1e0852134
Nurbu <lamanurbu2@gmail.com>
2026-05-06 14:09:56 -0400

    Fix issue with logic in prefix and firstName.

--- src/main/java/com/pluralsight/NameFormatter.java
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -25,39 +25,33 @@ public class NameFormatter {
 
     public static String format(String fullName) {
         String[] split = fullName.split(" ");
-        boolean prefix = false;
-        boolean middle = false;
-        boolean suffix = false;
-        String firstName = "";
+        String prefix = "";
+        boolean prefixFound = false;
+        String firstName = split[0];
         String lastName = "";
         String middleName = "";
+        String suffix = "";
 
         if (split.length == 2) {
             return split[1] + ", " + split[0];
         }
 
-        if (split[split[0].length() - 1] == ".") {
-            prefix = true;
+        if (split[0].charAt(split[0].length() - 1) == ('.')) {
+            prefix = split[0];
+            prefixFound = true;
             firstName = split[1];
         }
         int secondLast = split.length - 2;
         String value = split[secondLast];
         if (value.charAt(value.length() - 1) == ',') {
-            suffix = true;
+            suffix = split[split.length - 1];
             lastName = split[secondLast];
-            if (!(firstName.equals(split.length - 3))) {
-                middle = true;
+            if (!(firstName.equals(split[split.length - 3]))) {
                 middleName = split[secondLast - 1];
             }
         } else lastName = split[split.length - 1];
 
-        fullName += lastName + ", ";
-        if (prefix) fullName += split[0];
-        fullName += firstName + " ";
-        if (middle) fullName += middleName;
-        fullName += lastName;
-        if (suffix) fullName += split[secondLast] + 1;
-        return fullName;
+        return format(prefix, firstName, middleName, lastName, suffix);
 
     }
 
