f0a2d7588a986a1f15d6741fd4c5c65d9315ecff
Nurbu <lamanurbu2@gmail.com>
2026-05-06 11:39:30 -0400

    Made private constructor and finished the format method with String firstName and String lastName.

---
+++ src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class NameFormatter {
+    private NameFormatter() {
+    }
+
+    public static String format(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
+
+    
+}
+
